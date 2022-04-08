378dcd1cba72d144ad5d79e566c7c0e0c90d1ed6
pankaj <pankajsharma4x@gmail.com>
2022-04-08 10:53:02 +0530

    first portfolio

---
+++ src/data.js
@@ -0,0 +1,40 @@
+//Importing the images 
+import meme from "../src/img/meme.png";
+import routing from "../src/img/routing.png";
+import todo from "../src/img/todo.png";
+import weather from "../src/img/weather.png";
+import tinDog from "../src/img/tindog.png";
+import groove from "../src/img/groove.png"
+
+export const products = [
+    {
+      id: 1,
+      img:  meme,
+      link: "https://pankajsharma-4x.github.io/meme-generator-app/",
+    },
+    {
+      id: 2,
+      img: routing,
+      link: "https://pankajsharma-4x.github.io/react-routing/",
+    },
+    {
+      id: 3,
+      img: todo,
+      link: "https://pankajsharma-4x.github.io/ToDo-App/",
+    },
+    {
+      id: 4,
+      img:   weather,
+      link: "https://pankajsharma-4x.github.io/weather-update-app/",
+    },
+    {
+      id: 5,
+      img: tinDog,
+      link: "https://pankajsharma-4x.github.io/TinDog/",
+    },
+    {
+      id: 6,
+      img: groove,
+      link: "https://pankajsharma-4x.github.io/GROOV/",
+    },
+  ];
